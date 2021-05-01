5e6c07ab72e7f9467a83427d7fcb36bac2de104c
EvaSoltesova <soltesova.e@gmail.com>
2021-05-01 20:11:13 +0200

    Sablony seznam + detail

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,39 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class VizitkaController {
+
+  private final List<Vizitka> vizitky;
+
+  public VizitkaController() {
+    vizitky = Arrays.asList(
+            new Vizitka("Eva Šoltésová", "Questa", "Veterná 26", "917 01 Trnava", "soltesova.e@gmail.com", "0913 522 655", null ),
+            new Vizitka("Ruženka Šípková", "Dlhý spánok", "Kozácka 54", "917 02 Trnava", "ruzenka.sipkova@gmail.com", null, "www.dlhyspanok.sk"),
+            new Vizitka("Snehulienka Biela", "Sedem trpaslíkov", "Gondova 2", "811 02 Bratislava", null, "0910 155 211", "www.sedemtrpaslikov.sk")
+    );
+  }
   @GetMapping("/")
-  public String seznam() {
-    return "seznam";
+  public ModelAndView seznam() {
+    ModelAndView modelAndView = new ModelAndView("seznam");
+    modelAndView.addObject("vizitky", vizitky);
+    return modelAndView;
   }
 
-  @GetMapping("/detail")
-  public String detail() {
-    return "detail";
+  @GetMapping(value = "/detail", params = "id")
+  public ModelAndView detail(int id) {
+    ModelAndView modelAndView = new ModelAndView("detail");
+    modelAndView.addObject("vizitka", vizitky.get(id));
+    return modelAndView;
   }
 }
